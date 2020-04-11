271ebb67824cadc7aac3754e550bb20ea9f1f19e
David <d22mandu@gmail.com>
2020-04-11 14:05:50 -0700

    Changed to make motors run for 5s at one speed then stop and run at new speed for 5s

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include "MotorShield.h"
 
 int main() {
 	struct Motors motor1;
-
+	struct Motors motor2;
+	bool Yes = TRUE;
 	//***************************************************
 	//These ints are the number of the wiringPi library pins
 	//they correspond to pins 11, 15, and 13 on the actual Pi
@@ -11,7 +14,18 @@ int main() {
 	//in the pin description section it has these pins for Motor1
 	//****************************************************
    	Init(&motor1, 0, 3, 2);
-   	Move(&motor1, 'R');
+	Init(&motor2, 6, 5, 4);
+   	Move(&motor1, 'R', 50);
+	Move(&motor2, 'R', 50);
+	sleep(5);
+	Stop(Yes, &motor1);
+	Stop(Yes, &motor2);
+	sleep(5);
+	Move(&motor1, 'F', 25);
+	Move(&motor2, 'F', 25);
+	sleep(5);
+	Stop(Yes, &motor1);
+	Stop(Yes, &motor2);
 
    	return 0;
 }

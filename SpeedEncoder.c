3501410cf4b16bc6ecd26f03079862365f4cfe81
David <d22mandu@gmail.com>
2020-04-18 23:17:10 -0700

    Updated MeasureData method to take pointers to the speed of the wheels

--- SpeedEncoder.c
+++ SpeedEncoder.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <wiringPi.h>
 #include <pthread.h>
+#include <stdbool.h>
+#include "SpeedEncoder.h"
 
 #define PI 3.14
 
@@ -17,23 +19,26 @@ void SpeedEncoderInit(int pinNum) {
 
 void * SpeedEncoderMeasureData(void * args) {
 	printf("This is the SpeedEncoderThread. Now reading data\n");
+	struct MeasureDataArgs * argptr = (struct MeasureDataArgs *) args;
 	int pulses = 0;
-	int rotatioins = 0;
+	int rotations = 0;
 	int prevData = 0;
 	int data;
-	int * speed = (int *) args[0];
-	bool * movingPtr = (bool *) args[1];
-	while(movingPtr) {
+	int * speed = argptr->speedptr;
+	_Bool * moving = argptr->movingptr;
+	while(*moving) {
+		printf("\nTHREAD: isMoving: %d\n", *moving);
 		data = digitalRead(pin);
-		printf("\nMeasured Data: %d\n", data);
+		printf("THREAD: Measured Data: %d\n", data);
 		if(data != prevData) {
 			prevData = data;
 			++pulses;
 			rotations = pulses/18;
-			printf("\nPulses: %d  Rotations: %d\n", pulses, rotations);
+			printf("\nTHREAD: Pulses: %d  Rotations: %d\n", pulses, rotations);
 		}
-		++pulses;
-        *speed = 2*PI*pulses/3*18;
-		pthread_testcancel();
+        	*speed = 2*PI*pulses/3*18;
+		printf("THREAD: Speed: %d\n\n", *speed);
 	}
+	printf("THREAD: Wheels no longer moving, returning to main\n");
+	return 0;
 }
--- SpeedEncoder.h
+++ SpeedEncoder.h
@@ -4,6 +4,11 @@
 #ifndef SPEEDENCODER_H
 #define SPEEDENCODER_H
 
+struct MeasureDataArgs {
+	int * speedptr;
+	_Bool * movingptr;
+};
+
 void SpeedEncoderInit(int);
 
 void * SpeedEncoderMeasureData(void *);
